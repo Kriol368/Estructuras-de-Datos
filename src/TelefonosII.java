4b51a21644c3ca0dfeb10063ae0365d063462f85
Kriol368 <andreubeltran2004@gmail.com>
2023-12-11 09:48:44 +0100

    11/12/2023 09:48

--- src/TelefonosII.java
+++ src/TelefonosII.java
@@ -1,32 +1,23 @@
 import java.util.*;
 
 public class TelefonosII {
-    public static Map<String, ArrayList<Integer>> Numeros(){
-        Map<String, ArrayList<Integer>> numeros = new HashMap<>();
-        numeros.put("Pedro", (ArrayList<Integer>) Arrays.asList(691876654,654546372));
-        numeros.put("Juan", (ArrayList<Integer>) Arrays.asList(662537920,688333999,654456738));
-        numeros.put("Maria", (ArrayList<Integer>) Arrays.asList(619028564,678028000));
-        numeros.put("Manolo", (ArrayList<Integer>) List.of(692010222));
+    public static Map<String, List<String>> Numeros() {
+        Map<String, List<String>> numeros = new HashMap<>();
+        numeros.put("Pedro", Arrays.asList("691876654", "654546372"));
+        numeros.put("Juan", Arrays.asList("662537920", "688333999", "654456738"));
+        numeros.put("Maria", Arrays.asList("619028564", "678028000"));
+        numeros.put("Manolo", Arrays.asList("692010222"));
         return numeros;
     }
 
 
-    public static String Numeros(String contacto, Map<String, ArrayList<Integer>> numeros){
-        String numerosContacto = "";
-        ArrayList<Integer> numerosArray;
-        numerosArray = numeros.get(contacto);
-        for (int i = 0; i < numerosArray.size(); i++) {
-           numerosContacto += numerosArray.get(i) + ", ";
-        }
-        return numerosContacto;
-    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String contacto;
         System.out.println("Introduce un contacto: ");
         contacto = scanner.next();
-        System.out.println("Los numeros de " + contacto + " son " + Numeros(contacto,Numeros()));
-
+        Map<String, List<String>> numeros = Numeros();
+        System.out.println(numeros.get(contacto));
     }
 }
 
